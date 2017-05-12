7ee153d83c26ba551600dae24997113005b1e189
joshin-run <jahmsc@gmail.com>
2017-05-12 08:54:51 -0600

    Feat (barchart) Add barchart exersize; Test D3 connectivity

--- scripts/flow.js
+++ scripts/flow.js
@@ -9,3 +9,36 @@ $.ajax({
 })
 
 }
+//
+//
+// var myArrayOfObjects = [
+//         { x: 100, y: 100},
+//         { x: 130, y: 120},
+//         { x: 80 , y: 180},
+//         { x: 180, y: 80 },
+//         { x: 180, y: 40 }
+//       ];
+//
+//       myArrayOfObjects.forEach(function (d){
+//         console.log(d.x + ", " + d.y);
+//       });
+
+
+// d3.csv("data.csv", function (myArrayOfObjects){
+//   myArrayOfObjects.forEach(function (d){
+//     console.log(d.x + ", " + d.y);
+//   });
+// });
+
+
+// d3.csv("data.csv", type, function (myArrayOfObjects){
+//         myArrayOfObjects.forEach(function (d){
+//           console.log(d.x + d.y);
+//         });
+//       });
+//
+//       function type(d){
+//         d.x = parseFloat(d.x);
+//         d.y = parseFloat(d.y);
+//         return d;
+//       }
